627ffc53d8b558ae5a18a72fd59a649463b6cbef
goinfinity2 <goinfinity4747@gmail.com>
2026-01-27 14:09:54 +0200

    fix keys

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
 // ВСТАВЬ СЮДА СВОИ КЛЮЧИ ИЗ SUPABASE
 const SUPABASE_URL = 'https://sbnpevmtgynnglynrngp.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_PILiIHIeP-7kR4j6UWdmXA_2Zb5Z1UJ';
+const SUPABASE_ANON_KEY = 'sb_secret_gUZ9MSB1vTczMFzqYVYfwA_6BWN-nCw';
 
 // Инициализация Supabase
 const { createClient } = supabase;
